27a0258f45d5130a2b6fd48918117bbd202aa114
kevinbtian <kevinbtian@gmail.com>
2023-04-19 02:39:14 -0400

    adding custom handler

---
+++ kong/handler.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	// "database/sql"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type Handler struct {
+	// db *sql.DB
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) GetServicesHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		q := r.URL.Query().Get("q")
+		size, err := strconv.Atoi(r.URL.Query().Get("size"))
+		if err != nil {
+			http.Error(w, "invalid 'size' number", http.StatusBadRequest)
+		}
+		page, err := strconv.Atoi(r.URL.Query().Get("page"))
+		if err != nil {
+			http.Error(w, "invalid 'page' number", http.StatusBadRequest)
+		}
+
+		// Call database stuff here.
+		output := fmt.Sprintf("q: %s, size: %v, page: %v", q, size, page)
+		fmt.Fprintf(w, output)
+
+	default:
+		http.Error(w, "invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+}
--- kong/main.go
+++ kong/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/kevinbtian/interview/kong/handler"
 )
 
 func main() {
@@ -13,6 +14,9 @@ func main() {
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, test!")
 	})
+
+	h := handler.NewHandler()
+	http.HandleFunc("/service", h.GetServicesHandler)
 
 	fmt.Println("Starting server on hocalhost:8080...")
 	http.ListenAndServe(":8080", nil)
